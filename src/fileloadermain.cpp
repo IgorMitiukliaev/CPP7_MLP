5f33a13f3b3058f9876b24cf260c76dc0f8fe315
Didenko Ira <didenko.ira@gmail.com>
2022-08-06 15:19:35 +0700

    Добавлен метод для получения количества элементов выборки

--- src/fileloadermain.cpp
+++ src/fileloadermain.cpp
@@ -1,12 +1,14 @@
 #include "fileloader.cpp"
+#include <iostream>
 
 int main () {
-  std::string filename = "../datasets/emnist-letters-train.csv";
+  std::string filename = "../datasets/emnist-letters-test.csv";
   s21::FileLoader fileloader;
   fileloader.SetFileStream(filename);
+  std::cout << "Count of elements:" << fileloader.GetCountOfElements() << std::endl;
   while (fileloader.ReadElement()) {
     fileloader.PrintOutputValues(); // вывод результирующего вектора
-    fileloader.PrintInputValues(false); // вывод значениями
+  //  fileloader.PrintInputValues(false); // вывод значениями
     fileloader.PrintInputValues(true); // вывод звёздочками
     break;
   }
