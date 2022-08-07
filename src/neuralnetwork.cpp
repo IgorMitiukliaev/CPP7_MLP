b36a154d522e41c89db36e022fe46ecf41e3ad2d
wloyd <wloyd@student.21-school.ru>
2022-08-07 22:01:19 +0700

    added NeuralNetwork constructor (partly)

--- src/neuralnetwork.cpp
+++ src/neuralnetwork.cpp
@@ -1,18 +1,44 @@
 #include "neuralnetwork.h"
 
-using s21::NeuralNetwork, s21::NeuronGraph;
-NeuralNetwork::NeuralNetwork() {}
-NeuralNetwork::NeuralNetwork(initConfig config)
-    : num_layers_hidden(config.num_layers_hidden),
-      num_neurons_hidden(config.num_neurons_hidden){
+using s21::NeuralNetwork, s21::NeuronGraph, s21::Neuron;
 
-      };
+double Neuron::sigmoid(double x) { return 1 / (1 + exp(-x)); }
 
-NeuronGraph::NeuronGraph(){};
-
-NeuronGraph::NeuronGraph(std::vector<NeuronGraph*> n){};
-double NeuronGraph::sigmoid(double x) { return 1 / (1 + exp(-x)); }
-
-double NeuronGraph::activate() {
+double Neuron::activate(double input) {
+  for (int i = 0, res = 0; i < n.size(); i++) {
+    res += w[i] * (n[i] == nullptr ? n[i]->getResponse() : input);
+  }
+  res = sigmoid(res);
   return res;
 }
+
+double Neuron::getResponse() { return res; }
+
+NeuralNetwork::NeuralNetwork() {}
+
+NeuralNetwork::NeuralNetwork(initConfig config)
+    : num_layers_hidden(config.num_layers_hidden),
+      num_neurons_hidden(config.num_neurons_hidden) {
+  input_layer = std::vector<NeuronGraph>(config.num_neurons_input);
+  out_layer = std::vector<NeuronGraph>(config.num_neurons_out);
+
+  //  for (int i = 0; i < config.num_layers_input; i++) {
+  //  }
+};
+
+NeuronGraph::NeuronGraph()
+    : w(std::vector<double>(1)), n(std::vector<NeuronGraph *>(1)), res(.0) {
+  n[0] = nullptr;
+  w[0] = 1;
+};
+
+NeuronGraph::NeuronGraph(std::vector<NeuronGraph> *n_prev_layer) {
+  n = std::vector<NeuronGraph *>(n_prev_layer->size());
+  for (int i = 0; i < n.size(); i++) n[i] = &(*n_prev_layer)[i];
+  w = std::vector<double>(n_prev_layer->size());
+  std::random_device rd;
+  std::default_random_engine eng(rd());
+  std::uniform_real_distribution<double> distr(-1, 1);
+  std::for_each(w.begin(), w.end(),
+                [&distr, &eng](double &el) { el = distr(eng); });
+};
--- src/neuralnetwork.h
+++ src/neuralnetwork.h
@@ -1,39 +1,57 @@
 #ifndef NEURALNETWORK_H
 #define NEURALNETWORK_H
 
+#include <algorithm>
 #include <cmath>
+#include <random>
 #include <vector>
 
 namespace s21 {
 struct initConfig {
+  unsigned int num_neurons_input;
   unsigned int num_layers_hidden;
   unsigned int num_neurons_hidden;
+  unsigned int num_neurons_out;
   bool isGraph;
 };
+
+class Neuron {
+ private:
+  std::vector<double> w;
+  std::vector<Neuron *> n;
+  double res;
+  double sigmoid(double x);
+
+ public:
+  double activate(double input = 1);
+  double getResponse();
+};
+
+class NeuronGraph : public s21::Neuron {
+ public:
+  NeuronGraph();
+  NeuronGraph(std::vector<NeuronGraph> *n);
+  std::vector<double> getWeights();
+
+ private:
+  std::vector<double> w;
+  std::vector<NeuronGraph *> n;
+  double res;
+};
+
 class NeuralNetwork {
  public:
   NeuralNetwork();
   NeuralNetwork(initConfig config);
+  //  std::vector<s21::NeuronGraph> layer;
 
  private:
   unsigned int num_layers_hidden, num_neurons_hidden;
   unsigned int const num_neurons_input = 28 * 28, num_neurons_out = 26;
+  std::vector<NeuronGraph> input_layer;
+  std::vector<NeuronGraph> out_layer;
 };
 
-class NeuronGraph {
- public:
-  std::vector<double> getWeights();
-  double activate();
-  double getResponse();
-  NeuronGraph(std::vector<NeuronGraph*> n);
-
- private:
-  NeuronGraph();
-  std::vector<double> w;
-  std::vector<NeuronGraph*> n;
-  double res;
-  double sigmoid(double x);
-};
 }  // namespace s21
 
 #endif  // NEURALNETWORK_H
